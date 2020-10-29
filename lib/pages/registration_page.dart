0367f35d4a69b26cddbcf3cb5025d3517586e868
Teerasej <teerasej@gmail.com>
2020-10-29 20:55:38 +0700

    สร้าง layout แบบฟอร์ม

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -10,6 +10,44 @@ class RegisterationPage extends StatefulWidget {
 class _RegisterationPageState extends State<RegisterationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ลงทะเบียน'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('ชื่อ'),
+              TextFormField(),
+              SizedBox(
+                height: 15,
+              ),
+              Text('นามสกุล'),
+              TextFormField(),
+              SizedBox(
+                height: 15,
+              ),
+              Text('Email:'),
+              TextFormField(),
+              SizedBox(
+                height: 15,
+              ),
+              Text('รูปถ่าย:'),
+              Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('ลงทะเบียน'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
